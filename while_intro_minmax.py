b678c0d25a23a34cf0250ea20cb9e7f6dcd4e03f
angelaquintanas <angelaquintanas@gmail.com>
2017-03-22 00:36:20 -0500

    Tareas de while

--- while_intro_minmax.py
+++ while_intro_minmax.py
@@ -1,21 +1,24 @@
 #Python
 #Hecho por Angela QUintana
-#Fecha:14 marzo 2017
+#Fecha:22 marzo 2017
 
 numero= None
 contador= 0
 suma= 0
-media= 0
+minimo= None
+maximo= None
 
 while numero!= 'fin':
     numero= raw_input ('Introduzca un numero: ')
     try:
         contador= contador + 1
         suma= suma + int(numero)
+	if minimo == None or numero < minimo:
+		minimo= numero
+	if maximo==None or numero > maximo:
+		maximo= numero
     except ValueError:
 	if numero != 'fin':
 	    print "Entrada invalida"
 
-media= suma/contador
-print suma, contador, media
-
+print "Suma: ",suma, "Contador: ",contador, "Minimo: ", minimo, "Maximo: ",maximo
--- while_intro_number2.py
+++ while_intro_number2.py
@@ -7,14 +7,14 @@ contador= 0
 suma= 0
 media= 0
 
-while numero!= 'fin':
+while True:
     numero= raw_input ('Introduzca un numero: ')
+    if numero == "fin": break
     try:
         contador= contador + 1
         suma= suma + int(numero)
     except ValueError:
-	if numero != 'fin':
-	    print "Entrada invalida"
+	print "Entrada invalida"
 
 media= suma/contador
 print suma, contador, media
